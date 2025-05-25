e79e3630b1c37de0385c29bef8fcab63dc63c971
Antoni Tomaszewski <antek.tomaszewski@gmail.com>
2025-05-25 15:59:50 +0200

    Ustawia lint wyłącznie na nasze pliki
    a nie te z bibliotek

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "src/generated/**/*",
+      "node_modules/**/*",
+      ".next/**/*",
+      "out/**/*",
+      "coverage/**/*",
+      "prisma/migrations/**/*"
+    ]
+  }
 ];
 
 export default eslintConfig;
